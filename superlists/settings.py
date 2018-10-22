c124283c6dab84e6fcda15706eb8f0c0e7c93b0f
anyoneElse1 <anton.krivov@gmail.com>
2018-10-22 12:48:54 +0300

    Deletion of unused info.

--- superlists/settings.py
+++ superlists/settings.py
@@ -28,14 +28,14 @@ DEBUG = False
 ALLOWED_HOSTS = ['staging.talentfit.net']
 
 
-if 'DJANGO_DEBUG_FALSE' in os.environ:
-    DEBUG = False
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
-else:
-    DEBUG = True
-    SECRET_KEY = 'insecure-key-for-dev'
-    ALLOWED_HOSTS = []
+#if 'DJANGO_DEBUG_FALSE' in os.environ:
+#    DEBUG = False
+#    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+#    ALLOWED_HOSTS = [os.environ['SITENAME']]
+#else:
+#    DEBUG = True
+#    SECRET_KEY = 'insecure-key-for-dev'
+#    ALLOWED_HOSTS = []
 
 # Application definition
 
